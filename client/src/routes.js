06183f4a91878061e15920640d5701fd197b0cd8
surekhag <surekhag@github.com>
2020-02-14 17:12:39 +0530

    Removed unwanted code

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,7 +1,6 @@
 import Dashboard from '@material-ui/icons/Dashboard'
 import Person from '@material-ui/icons/Person'
 import AssignmentLateIcon from '@material-ui/icons/AssignmentLate'
-import AssignmentIcon from '@material-ui/icons/Assignment';
 import HomeWorkIcon from '@material-ui/icons/HomeWork'
 import GroupIcon from '@material-ui/icons/Group';
 import DashboardPage from './views/Dashboard/Dashboard.js'
@@ -9,8 +8,6 @@ import UserProfile from './views/UserProfile/UserProfile.js'
 import TableList from './views/TableList/TableList.js'
 import LeaveForm from './views/LeaveForm/LeaveForm.js'
 import WFHForm from './views/WFHForm/WFHForm.js'
-import Projects from './views/Project/Project';
-import Employee from './views/Employee/Employee';
 
 const dashboardRoutes = [
     {
@@ -37,23 +34,7 @@ const dashboardRoutes = [
         component: TableList,
         layout: '/admin'
     },
-    {
-        path: '/employee',
-        name: 'Employees',
-        rtlName: 'ملف تعريفي للمستخدم',
-        icon: GroupIcon,
-        component: Employee,
-        layout: '/admin'
-    },
-    {
-        path: '/projects',
-        name: 'Projects',
-        rtlName: 'قائمة الجدول',
-        icon: AssignmentIcon,
-        component: Projects,
-        layout: '/admin'
-    },
-    {
+     {
         path: '/LeaveForm',
         name: 'Leave Request',
         rtlName: 'قائمة الجدول',
