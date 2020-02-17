c45b749186cbbf227e3682b7fd4394ec2d8a0235
surekhag <surekhag@github.com>
2020-02-17 15:35:40 +0530

    Added employee and Project code -in progress

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,6 +2,10 @@ import Dashboard from '@material-ui/icons/Dashboard'
 import Person from '@material-ui/icons/Person'
 import DashboardPage from './views/Dashboard/Dashboard.js'
 import UserProfile from './views/UserProfile/UserProfile.js'
+import GroupIcon from '@material-ui/icons/Group';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import Projects from './views/Project/Project';
+import Employee from './views/Employee/Employee';
 
 const dashboardRoutes = [
     {
@@ -19,7 +23,24 @@ const dashboardRoutes = [
         icon: Person,
         component: UserProfile,
         layout: '/admin'
-    }
+    },
+    {
+        path: '/employee',
+        name: 'Employees',
+        rtlName: 'ملف تعريفي للمستخدم',
+        icon: GroupIcon,
+        component: Employee,
+        layout: '/admin'
+    },
+    {
+        path: '/projects',
+        name: 'Projects',
+        rtlName: 'قائمة الجدول',
+        icon: AssignmentIcon,
+        component: Projects,
+        layout: '/admin'
+    },
+
 ]
 
 export default dashboardRoutes
