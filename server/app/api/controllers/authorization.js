30af4efbd378121e64142e349a6457e534dda1b4
surekhag <surekhag@github.com>
2020-02-19 16:53:41 +0530

    Intermediate commit

--- server/app/api/controllers/authorization.js
+++ server/app/api/controllers/authorization.js
@@ -1,15 +1,17 @@
 const userModel = require("../models/users");
 module.exports = {   
 getAuth: function(req, res, next) {
-    userModel.find({_id: req.user.id}, function(err, users) {
+
+    userModel.findOne({_id: req.user.id}, function(err, users) {
       if (err) {
           console.log("here in err");
         next(err);
       } else {
+        const { password, ...userWithoutPassword } = users._doc;       
         res.json({
           status: "success",
           message: "User found!!!",
-          data: users
+          data: { user : userWithoutPassword }
         });
       }
     });
--- server/routes/peer-review.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const peerReviewController = require("../app/api/controllers/peer-review");
-
-// router.get("/", peerReviewController.getAll);
-router.post("/add", peerReviewController.create);
-// router.put("/:id", peerReviewController.update);
-// router.delete("/:id", peerReviewController.delete);
-module.exports = router;
