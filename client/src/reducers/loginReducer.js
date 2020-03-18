b3fa03212659f69fe2db39fbe0251abcc1ff44d5
surekhag <surekha.gadkari@objectedge.com>
2020-03-18 10:34:10 +0530

    Login code optimization

--- client/src/reducers/loginReducer.js
+++ client/src/reducers/loginReducer.js
@@ -2,7 +2,8 @@ import {
   LOGIN_TO_SITE_SUCCESS,
   LOGIN_TO_SITE_ERROR,
   SESSION_EXPIRED,
-  LOGOUT_FROM_SITE
+  LOGOUT_FROM_SITE,
+  CLEAR_LOGIN_ERRORS
 } from '../actions/actionTypes.js'
 
 const initialState = {
@@ -27,20 +28,25 @@ const loginReducer = (state = initialState, action) => {
         isLoading: false,
         error: action.message
       }
-      case SESSION_EXPIRED:        
-        return {
-          ...state,
-          isLoading: false,
-          currentUser: null,
-          error: "Session has been expired! Please try again. ",
-        }
-        case LOGOUT_FROM_SITE:          
-          return{
-            ...state,
-            currentUser: null,
-            error: null,
-            isLoading: false
-          }
+    case SESSION_EXPIRED:
+      return {
+        ...state,
+        isLoading: false,
+        currentUser: null,
+        error: 'Session has been expired! Please try again. '
+      }
+    case LOGOUT_FROM_SITE:
+      return {
+        ...state,
+        currentUser: null,
+        error: null,
+        isLoading: false
+      }
+    case CLEAR_LOGIN_ERRORS:
+      return {
+        ...state,
+        error: null
+      }
     default:
       return state
   }
--- client/src/sagas/index.js
+++ client/src/sagas/index.js
@@ -1,7 +1,7 @@
 import { all, fork } from 'redux-saga/effects'
 import watchLogoutSaga from './watchers/logout.js'
 import watchEmployeeInfoSaga from './watchers/employeeInfo.js'
-import watchLoginSaga, { watchAuthenticateSaga } from './watchers/login'
+import { watchLoginSaga, watchAuthenticateSaga } from './watchers/login'
 
 import watchPeerReviewSaga from './watchers/peerReview'
 import watchLoadAllProjects from './watchers/project'
