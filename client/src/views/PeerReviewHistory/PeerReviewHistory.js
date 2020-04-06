9e177d1baba7c2b67138fc954c6bf36e3eb34ff8
RakshandaWankhede <rakshanda.wankhede@objectedge.com>
2020-04-06 15:53:14 +0530

    Peer Review History UI-inProgess

--- client/src/views/PeerReviewHistory/PeerReviewHistory.js
+++ client/src/views/PeerReviewHistory/PeerReviewHistory.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { loadAllPeerForUser } from '../../actions/peerReviewAction'
-
+import { loadAllPeerForUser, loadAllPeerReviews } from '../../actions/peerReviewAction'
+import GroupIcon from '@material-ui/icons/Group'
+import Person from '@material-ui/icons/Person'
 // react plugin for creating charts
-// @material-ui/core
-import { makeStyles } from '@material-ui/core/styles'
 
 // @material-ui/icons
 
@@ -14,24 +13,20 @@ import GridContainer from '../../components/Grid/GridContainer'
 import Button from '../../components/CustomButtons/Button'
 import { Dialog, DialogActions } from '@material-ui/core'
 import Table from '../../components/Table/Table'
-import Card from '../../components/Card/Card'
-import CardHeader from '../../components/Card/CardHeader'
-import CardBody from '../../components/Card/CardBody'
+import CustomTabs from "../../components/CustomTabs/CustomTabs";
 import PeerReviewDetails from '../../components/PeerReviewDetails/PeerReviewDetails'
-import styles from '../../assets/jss/material-dashboard-react/views/dashboardStyle'
 import withAuth from '../../HOC/withAuth'
 import { UserContext } from '../../context-provider/user-context'
 import { formatDate } from '../../helpers/formatDates'
 import {
     userPeerReview,
+    peerReviewDataSelector
 } from '../../selectors/reviewSelectors'
 
-const useStyles = makeStyles(styles)
-
 const PeerReviewHistory = props => {
-    const classes = useStyles()
     const dispatch = useDispatch()
     const peerReviews = useSelector(userPeerReview)
+    const peerReviewData = useSelector(peerReviewDataSelector)
     const [peerDetails, setPeerDetails] = useState('')
     const { currentUser } = useContext(UserContext)
     const [showDetail, setShowDetail] = useState(false)
@@ -42,10 +37,38 @@ const PeerReviewHistory = props => {
         'Due Date',
         'Status'
     ]
+    const employeeReviewListingHeader = [
+        'Employee Under Review',
+        'Employee Reviewing',
+        'Project',
+        'Due Date',
+        'Status'
+    ]
     useEffect(() => {
         dispatch(loadAllPeerForUser(currentUser._id))
+        if (currentUser.userRole === "manager")
+            dispatch(loadAllPeerReviews())
     }, [currentUser._id, dispatch])
 
+    const employeePeerReviewArr = []
+    let filteredEmployeeReview
+    if (currentUser.userRole === "manager" && peerReviewData) {
+        filteredEmployeeReview = peerReviewData.data.data.filter(
+            cls =>
+                currentUser._id === cls.functional_manager._id &&
+                cls.status === 'Done'
+        )
+        filteredEmployeeReview.map((review, key) => {
+            employeePeerReviewArr.push([
+                `${review.employee_under_review.firstname} ${review.employee_under_review.lastname}`,
+                `${review.employee_reviewing.firstname} ${review.employee_reviewing.lastname}`,
+                review.project.title,
+                formatDate(review.to_date),
+                review.status
+            ])
+            return 1
+        })
+    }
     const peerReviewsArray = []
     let filteredReview
     if (peerReviews) {
@@ -68,27 +91,50 @@ const PeerReviewHistory = props => {
         setPeerDetails(filteredReview[key])
         setShowDetail(true)
     }
+    const onClickEmployeeDetailsHandler = key => {
+        console.log("hey", filteredEmployeeReview, key)
+        setPeerDetails(filteredEmployeeReview[key])
+        setShowDetail(true)
+    }
     return (
         <GridContainer>
-            {peerReviews && peerReviewsArray.length > 0 ? (
-                <GridItem xs={12} sm={12} md={12}>
-                    <Card plain>
-                        <CardHeader plain color="primary">
-                            <h4 className={classes.cardTitleWhite}>PEER REVIEWS</h4>
-                        </CardHeader>
-                        <CardBody>
-                            <Table
-                                tableHeaderColor="gray"
-                                tableHead={peerReviewListingHeader}
-                                tableData={peerReviewsArray || null}
-                                showLink={true}
-                                buttonText="Details"
-                                onClickHandler={onClickHandler}
-                            />
-                        </CardBody>
-                    </Card>
-                </GridItem>
-            ) : null}
+            <GridItem>
+                <CustomTabs
+                    title=""
+                    headerColor="primary"
+                    tabs={[
+                        {
+                            tabName: "My Reviews",
+                            tabIcon: Person,
+                            tabContent: (
+                                <Table
+                                    tableHeaderColor="gray"
+                                    tableHead={peerReviewListingHeader}
+                                    tableData={peerReviewsArray || null}
+                                    showLink={true}
+                                    buttonText="Details"
+                                    onClickHandler={onClickHandler}
+                                />
+                            )
+                        },
+                        currentUser.userRole === "manager" ? {
+                            tabName: "Employee Reviews",
+                            tabIcon: GroupIcon,
+                            tabContent: (
+                                <Table
+                                    tableHeaderColor="gray"
+                                    tableHead={employeeReviewListingHeader}
+                                    tableData={employeePeerReviewArr || null}
+                                    showLink={true}
+                                    buttonText="Details"
+                                    onClickHandler={onClickEmployeeDetailsHandler}
+                                />
+                            )
+                        } : {}
+
+                    ]}
+                />
+            </GridItem>
             <Dialog
                 title="Peer Review Details"
                 maxWidth="lg"
@@ -107,8 +153,7 @@ const PeerReviewHistory = props => {
                             size="sm"
                             onClick={() => setShowDetail(false)}
                         >
-                            {' '}
-              Close
+                            Close
             </Button>
                     </GridItem>
                 </DialogActions>
