2616e12439bb674862dc516becdd568a48ae8c72
Aatish <aatish.bondge@objectedge.com>
2020-04-09 16:47:33 +0530

    Employee sorting at server side

--- server/app/api/controllers/users.js
+++ server/app/api/controllers/users.js
@@ -117,10 +117,7 @@ module.exports = {
   },
   getAll: function (req, res, next) {
     const query = req.query || {}
-    userModel.find(query, function (err, users) {
-      users.sort((a, b) =>
-      a.status.toLowerCase() < b.status.toLowerCase() ? -1 : 1
-    )
+    userModel.find(query, null,{sort: 'status'}, function (err, users) {
       if (err) {
         next(err);
       } else {
